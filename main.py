f7d64b91167a1a0c320d947b7233f92a119164cd
DinilR <dinilsratnayake@gmail.com>
2023-11-17 01:19:39 +0530

    adding directory selection

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
 from PyQt5.QtGui import QIcon
 from application import Ui_MainWindow
 import subprocess
+import os
 
 class my_app(QMainWindow):
     def __init__(self):
         super(my_app,self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
+        project_location = os.path.dirname(os.path.abspath(__file__))
+        self.ui.path_text.setText(project_location)
+
         # self.setGeometry(400,150,500,500)
         # self.setWindowTitle("EMvidence")
         # self.setWindowIcon(QIcon("logo.png"))
         # self.setToolTip("EMvidence")
 
         self.ui.button1.clicked.connect(self.collect_data)
+        self.ui.path_button.clicked.connect(self.path_specify)
+
+    def path_specify(self):
+        folder_path = QFileDialog.getExistingDirectory(self,"Select Directory: ",self.ui.path_text.text())
+        if folder_path:
+            self.ui.path_text.setText(folder_path)
+            self.project_location = folder_path
+            print("Selected Path: "+folder_path)
+
 
     def collect_data(self):
         print("Button Clicked")
@@ -29,7 +42,8 @@ class my_app(QMainWindow):
         print(time)
 
         
-        args = ["python", "EM.py", "--samp_rate", samp_rate, "--cent_freq", cent_freq_value + cent_freq_scale, "--time", time]
+        args = ["python", "EM.py", "--samp_rate", samp_rate, "--cent_freq", cent_freq_value + cent_freq_scale, "--time", time,"--file",self.project_location]
+        print("Location: "+self.project_location)
         try:
             result = subprocess.run(args, capture_output=True, text=True, check=True)
             print("Subprocess output:")
@@ -39,6 +53,9 @@ class my_app(QMainWindow):
             print(f"Subprocess failed with return code {e.returncode}")
             if e.returncode == 3221225477:
                 print("Check if HackRF is connected")
+            else:
+                print("Error output:")
+                print(e.stderr)    
             # print("Error output:")
             # print(e.stderr)   
     
